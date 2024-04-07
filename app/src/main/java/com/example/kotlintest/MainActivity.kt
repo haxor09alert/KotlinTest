d6a427fbe792c2ecf50ee09f5de1956da6439ed5
Alekh Chaudhary <chaudharyalekh@gmail.com>
2024-04-07 16:24:08 +1000

    Kotlin Test Project

---
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.kotlintest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlintest.ui.theme.KotlinTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = Color.Gray) {
+                    Greeting("Meow")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Ni Hao $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinTestTheme {
+        Greeting("Android")
+    }
+}
+
+
+// Login Page
